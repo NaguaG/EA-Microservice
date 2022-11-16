a901ef4f2bb0cc6a0549ec0995dbe3b3d21c9eab
NaguaG <82986724+NaguaG@users.noreply.github.com>
2022-11-15 23:58:27 -0600

    Add files via upload

---
+++ product-service/target/classes/data.sql
@@ -0,0 +1,2 @@
+insert into product (id, name, color) values (1, 'Product 1', 'blue');
+insert into product (id, name, color) values (2, 'Product 2', 'red');
---
+++ user-service/target/classes/data.sql
@@ -0,0 +1,2 @@
+insert into  users (id,email,full_name,password) VALUES (1,'uinan@,miu.edu','uinan','123456');
+insert into  users (id,email,full_name,password) VALUES (2,'jdoe@,miu.edu','jdoe','123456');
